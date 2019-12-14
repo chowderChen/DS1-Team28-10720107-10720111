925ebd012a04829b3eeb539b37eaf4514918ed20
chowderChen <55129325+chowderChen@users.noreply.github.com>
2019-12-14 12:26:14 +0800

    able to load file correctly

--- Ex6/DS1ex6_34_10720111_10720107.cpp
+++ Ex6/DS1ex6_34_10720111_10720107.cpp
@@ -1,36 +1,51 @@
-// 10720111 Â³Â¯Â¤Ã–Â·u 10720107 Â³Â¯Â¥AÂ¤Â¤
+// 10720111 ³¯¤Ö·u 10720107 ³¯¥A¤¤
 
 #include<iostream> 
 #include<cstdio>
+#include<cstdlib>
 #include<vector>
 #include<string>
 
 using namespace std ;
 
+typedef struct CT {
+	string nameSchool ; // All the content from the school
+	string nameMajor ;
+	string division ;
+	string level ;
+	int numStudent ;
+	int numTeacher ;
+	int numGraduate ; // number of graduates
+} CollegeType ;
+
 class ClassList {
-	
-	typedef struct CT {
-		string content ; // All the content from the school
-		int numGraduate ; // number of graduates
-	} CollegeType ;
 	
 	vector< CollegeType > collegeSet ; 
 	
 	public:
 		
 	ClassList(){
-		fileNum = "" ;
-		sortType = "" ;
-		sortTime = 0 ;
+		
 	} // ClassList
 	
 	bool Load( string fileName ) ;
+	void Print() ;
 };
+
+void ClassList::Print() {
+	
+	for ( int i = 0 ; i < collegeSet.size() ; i++ ) {
+			cout << collegeSet[i].nameSchool << "\t" << collegeSet[i].nameMajor << "\t"
+				 << collegeSet[i].division << "\t" << collegeSet[i].level << "\t" 
+				 << collegeSet[i].numStudent << "\t" << collegeSet[i].numTeacher << "\t"
+				 << collegeSet[i].numGraduate << "\n";
+	} // for
+		
+} // Print
 
 bool ClassList::Load( string fileName ) {
     // Load a file, turn it into a vector
     
-    fileNum = fileName ;
     FILE *infile = NULL ;
     bool success = false ;
     fileName = "input" + fileName + ".txt" ;
@@ -46,33 +61,60 @@ bool ClassList::Load( string fileName ) {
             	;
 
         CollegeType aCollege ;
-        aCollege.content = "" ;
+        aCollege.nameSchool = "" ;
+        aCollege.nameMajor = "" ;
+        aCollege.division = "" ;
+        aCollege.level = "" ;
+        aCollege.numStudent = 0 ;
+        aCollege.numTeacher = 0 ;
         aCollege.numGraduate = 0 ;
         
 		int numTab = 0 ;
-		string numGraduate = "" ;
+		string stringStudent = "" ;
+		string stringTeacher = "" ;
+		string stringGraduate = "" ;
 		
         while ( fscanf( infile, "%c", &tempChar ) != EOF ) {
-        	aCollege.content = aCollege.content + tempChar ;
         	while ( fscanf( infile, "%c", &tempChar ) != EOF && tempChar != '\n' ) {
-        		aCollege.content = aCollege.content + tempChar ;
         		if ( tempChar == '\t' )
         			numTab++ ;
+        		else if ( numTab == 1 )
+        			aCollege.nameSchool = aCollege.nameSchool + tempChar ;
+        		else if ( numTab == 3 )
+        			aCollege.nameMajor = aCollege.nameMajor + tempChar ;
+        		else if ( numTab == 4 )
+        			aCollege.division = aCollege.division + tempChar ;
+        		else if ( numTab == 5 )
+        			aCollege.level = aCollege.level + tempChar ;
+        		else if ( numTab == 6 )
+        			stringStudent = stringStudent + tempChar ;
+        		else if ( numTab == 7 )
+        			stringTeacher = stringTeacher + tempChar ;
         		else if ( numTab == 8 )
-        			numGraduate = numGraduate + tempChar ;
+        			stringGraduate = stringGraduate + tempChar ;
         		
         	} //while
         		
-        	aCollege.numGraduate = atoi( numGraduate.c_str() ) ;
+        	aCollege.numStudent = atoi( stringStudent.c_str() ) ;
+        	aCollege.numTeacher = atoi( stringTeacher.c_str() ) ;
+        	aCollege.numGraduate = atoi( stringGraduate.c_str() ) ;
         	
             collegeSet.push_back( aCollege ) ;
             
-            aCollege.content = "" ;
-            aCollege.numGraduate = 0 ;
-            numGraduate = "" ;
+            aCollege.nameSchool = "" ;
+	        aCollege.nameMajor = "" ;
+	        aCollege.division = "" ;
+	        aCollege.level = "" ;
+	        aCollege.numStudent = 0 ;
+	        aCollege.numTeacher = 0 ;
+	        aCollege.numGraduate = 0 ;
+	        
+            stringStudent = "" ;
+            stringTeacher = "" ;
+            stringGraduate = "" ;
             numTab = 0 ;
             
-        } // while
+        } // while not EOF
 
         success = true ;
         fclose( infile ) ;
@@ -83,12 +125,8 @@ bool ClassList::Load( string fileName ) {
 
 } // Load
 
-struct TreeType {
-	
-};
-
 struct TreeNode {
-	TreeType item ;
+	CollegeType content ;
 	TreeNode *left ;
 	TreeNode *right ;
 };
@@ -101,7 +139,8 @@ class Tree {
 int main() {
 	
 	ClassList classList ;
-	classList.Load() ;
+	classList.Load( "601" ) ;
+	classList.Print() ;
 	
 	return 0 ;
 	
